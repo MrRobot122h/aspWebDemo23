cbb95d1bdfdff44cb5b4056049a5685fe3a4ebde
DESKTOP-6I9A62J\doski <d.oskin2003@gmail.com>
2024-07-07 12:37:35 +0300

    add controler culc

---
+++ Demo2/Demo2/Controllers/CalcController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet("Add")]
+        public ActionResult<double> AddValues(double num, double number)
+        {
+            var result = num + number;
+            return result;
+        }
+
+        [HttpGet("Substract")]
+        public ActionResult<double> SubstractValues(double num, double number)
+        {
+            var result = num - number;
+            return result;
+        }
+
+        [HttpGet("Divide")]
+        public ActionResult<double> DivideValues(double num, double number)
+        {
+            var result = num / number == 0 ? 1: number;
+            return result;
+        }
+
+
+        [HttpGet("Multiply")]
+        public ActionResult<double> MultiplyValues(double num, double number)
+        {
+            var result = num * number;
+            return result;
+        }
+    }
+}
