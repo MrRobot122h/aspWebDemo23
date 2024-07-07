471eaef7cb3d5b591633739f4cd89bf19a562988
DESKTOP-6I9A62J\doski <d.oskin2003@gmail.com>
2024-07-07 12:54:09 +0300

    add DataControler

---
+++ Demo2/Demo2/Controllers/DataController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        // POST api/postexample
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+
+            return Ok($"Отримано значення: {value}");
+        }
+
+        // POST api/postexample/data
+        [HttpPost("data")]
+        public ActionResult<object> PostData([FromBody] DataModel data)
+        {
+
+            return Ok(new { Message = "Дані успішно отримано", Data = data });
+        }
+    }
+}
